798a1e55ace596b888288f031b868b0521f3071e
Saurav1872 <saurav.suman1807@gmail.com>
2025-06-22 18:26:22 +0530

    edited deployment config

---
+++ backend/src/main/java/com/bank/BankingSystem/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.bank.BankingSystem.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> checkHealth() {
+        return ResponseEntity.ok("running");
+    }
+} 
